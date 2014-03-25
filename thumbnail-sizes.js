89980b9de7bb488d07d8826ca2178e4e576c3207
Matthew Dean <matthewdean@users.noreply.github.com>
2014-03-25 00:15:20 -0500

    Update thumbnail-sizes.js

--- thumbnail-sizes.js
+++ thumbnail-sizes.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var async = require('async');
 
 var sizes = [
   { width: 1, height: 1 }, // intentionally invalid
@@ -14,34 +15,67 @@ var sizes = [
   { width: 420, height: 420 }
 ];
 
-var apis = [
-  {
-		name: "/Game/Tools/ThumbnailAsset.ashx",
-		url: "http://www.roblox.com/Game/Tools/ThumbnailAsset.ashx?aid=1818&fmt=png",
-		width_param: "wd",
-		height_param: "ht"
-	},
-	{
-		name: "/Thumbs/Pixelated.ashx",
-		url: "http://www.roblox.com/Thumbs/Pixelated.ashx?id=1818&format=Png&tfid=114",
+var apis = [{
+	name: "/Game/Tools/ThumbnailAsset.ashx",
+	url: "http://www.roblox.com/Game/Tools/ThumbnailAsset.ashx?aid=1818&fmt=png",
+	width_param: "wd",
+	height_param: "ht"
+}, {
+	name: "/Thumbs/Pixelated.ashx",
+	url: "http://www.roblox.com/Thumbs/Pixelated.ashx?id=1818&format=Png&tfid=114",
+	width_param: "x",
+	height_param: "y"
+}, {
+	name: "/Asset-Thumbnail/Json",
+	url: "http://www.roblox.com/Asset-Thumbnail/Json?assetId=1818&format=jpeg",
+	width_param: "width",
+	height_param: "height"
+}, {
+	name: "/Outfit-Thumbnail/Json",
+	url: "http://www.roblox.com/Outfit-Thumbnail/Json?userOutfitId=2&format=png",
+	width_param: "width",
+	height_param: "height"
+}, {
+	name: "/Thumbs/Asset.ashx",
+	url: "http://www.roblox.com/Thumbs/Asset.ashx?AssetID=1818",
+	width_param: "width",
+	height_param: "height"
+}, {
+	name: "/Thumbs/RawAsset.ashx",
+	url: "http://www.roblox.com/Thumbs/Asset.ashx?assetId=1818&imageFormat=png",
+	width_param: "width",
+	height_param: "height"
+}, {
+		name: "Avatar.ashx",
+		url: "http://www.roblox.com/Thumbs/Avatar.ashx?username=Shedletsky&format=png",
 		width_param: "x",
 		height_param: "y"
-	},
-	{
-		name: "/Asset-Thumbnail/Json",
-		url: "http://www.roblox.com/Asset-Thumbnail/Json?assetId=1818&format=jpeg",
-		width_param: "width",
-		height_param: "height"
-	}
-];
+}];
 
-sizes.forEach(function(size) {
-	apis.forEach(function(api) {
-		var qs = {};
-		qs[api.width_param] = size.width;
-		qs[api.height_param] = size.height;
-		request.get(api.url, { qs: qs }, function(err, res) {
-			console.log(res.statusCode, api.name, size.width + 'x' +  size.height);
-		});
+var q = async.queue(function (task, callback) {
+	var qs = {};
+	qs[task.width_param] = task.width;
+	qs[task.height_param] = task.height;
+
+	var width = task.width;
+	var height = task.height;
+	request.get(task.url, { qs: qs }, function(err, res) {
+		if (res.statusCode == 200) {
+				console.log(task.name, width + 'x' +  height);
+		}
+		callback();
+	});
+}, 2);
+
+apis.forEach(function(api) {
+	sizes.forEach(function(size) {
+		var task = {};
+		task.name = api.name;
+		task.url = api.url;
+		task.width = size.width;
+		task.height = size.height;
+		task.width_param = api.width_param;
+		task.height_param = api.height_param;
+		q.push(task);
 	});
 });
